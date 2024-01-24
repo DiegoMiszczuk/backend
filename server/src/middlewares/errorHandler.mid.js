aa8f6208a146692710d419836f87e12b0a2bad78
Diego Miszczuk <diegohmiszczuk@gmail.com>
2024-01-24 10:41:07 -0300

    corrijo error Handler

--- server/src/middlewares/errorHandler.mid.js
+++ server/src/middlewares/errorHandler.mid.js
@@ -1,7 +1,7 @@
 function errorHandler(error, req, res, next) {
   console.log(error);
-  return res.json({
-    statusCode: error.statusCode || 500,
+  return res.status( error.statusCode || 500).json({
+    status: error.statusCode || 500,
     message: `${req.method} ${req.url} ${error.message}`,
   });
 }
